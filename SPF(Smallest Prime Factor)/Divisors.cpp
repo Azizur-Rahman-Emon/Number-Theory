4f341838fee1ece302645aa82da68c7f40864644
Azizur-Rahman-Emon <azizurrahmanemoncs13@gmail.com>
2026-03-18 16:43:05 +0600

    Create Divisors.cpp

---
+++ SPF(Smallest Prime Factor)/Divisors.cpp
@@ -0,0 +1,23 @@
+vector<int> getDivisors(int n) {
+    vector<int> divs = {1};
+    while (n > 1) {
+        int p = spf[n];
+        int cnt = 0;
+        while (n % p == 0) {
+            n /= p;
+            cnt++;
+        }
+
+        vector<int> tmp;
+        for (int d : divs) {
+            long long val = 1;
+            for (int i = 0; i < cnt; i++) {
+                val *= p;
+                tmp.push_back(d * val);
+            }
+        }
+        for (int x : tmp) divs.push_back(x);
+    }
+    sort(divs.begin(), divs.end());
+    return divs;
+}
