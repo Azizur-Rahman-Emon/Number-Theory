83c5053a54a843d74bd5ded94514ffa686ebf441
Azizur-Rahman-Emon <azizurrahmanemoncs13@gmail.com>
2026-06-20 12:55:55 +0600

    Update Prime_factorization_by spf.cpp

--- SPF(Smallest Prime Factor)/Prime_factorization_by spf.cpp
+++ SPF(Smallest Prime Factor)/Prime_factorization_by spf.cpp
@@ -1,18 +1,33 @@
-vector<long long> primeFactors(long long n)
-{
-    vector<long long> factors;
+const int N = 1e6 + 5;
+int spf[N];
 
-    for (long long i = 2; i * i <= n; i++)
+void build_spf()
+{
+    for (int i = 1; i < N; i++)
+        spf[i] = i;
+
+    for (int i = 2; i * i < N; i++)
     {
-        while (n % i == 0)
+        if (spf[i] == i)
         {
-            factors.push_back(i);
-            n /= i;
+            for (int j = i * i; j < N; j += i)
+            {
+                if (spf[j] == j)
+                    spf[j] = i;
+            }
         }
     }
+}
 
-    if (n > 1)
-        factors.push_back(n);
+vector<int> prime_factorization(int n)
+{
+    vector<int> factors;
+
+    while (n > 1)
+    {
+        factors.push_back(spf[n]);
+        n /= spf[n];
+    }
 
     return factors;
 }
