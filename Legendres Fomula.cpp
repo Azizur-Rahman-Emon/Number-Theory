efcd6ada7416506af22495905527f8b828448692
Azizur-Rahman-Emon <azizurrahmanemoncs13@gmail.com>
2026-07-19 01:59:18 +0600

    Create Legendres Fomula.cpp

---
+++ Legendres Fomula.cpp
@@ -0,0 +1,18 @@
+//Find the largest x such that p^x divides n!
+#include<bits/stdc++.h>
+using namespace std;
+
+int legendre(long long n, long long p) {
+  int ans = 0;
+  while (n) {
+    ans += n / p;
+    n /= p;
+  }
+  return ans;
+}
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  return 0;
+}
